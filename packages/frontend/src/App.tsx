309638ea21ebf4b68a54188c0fbd6751975bf7f9
sarangspadalkar <padalkar.sarang@gmail.com>
2024-02-19 00:07:53 -0700

    initial setup of react app

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,24 +1,26 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import UserDisplay from "./components/UserDisplay";
+import { User } from "./types";
 
 function App() {
+  const users: User[] = [
+    {
+      name: "Prisma Fan",
+      messages: [
+        {
+          body: "Prisma rocks!!",
+        },
+        {
+          body: "Did I mention I love Prisma?",
+        },
+      ],
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+      {users.map((user, i) => (
+        <UserDisplay user={user} key={i} />
+      ))}
     </div>
   );
 }
