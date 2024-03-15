2cde3525e419bd6fda052d5943a2429523379dd1
sarangspadalkar <padalkar.sarang@gmail.com>
2024-03-15 08:51:32 -0600

    add string and int comparison input types

--- packages/backend/src/builder.ts
+++ packages/backend/src/builder.ts
@@ -24,4 +24,35 @@ export const emailScalar = builder.addScalarType(
   EmailAddressResolver,
   {}
 );
+
+export const stringInput = builder.inputType('StringInput', {
+  fields: (t) => ({
+    ne: t.string(),
+    eq: t.string(),
+    le: t.string(),
+    lt: t.string(),
+    ge: t.string(),
+    gt: t.string(),
+    in: t.stringList(),
+    contains: t.string(),
+    not_contains: t.string(),
+    like: t.string(),
+    not_like: t.string(),
+    startsWith: t.string(),
+    endsWith: t.string()
+  })
+});
+
+export const intInput = builder.inputType('IntInput', {
+  fields: (t) => ({
+    ne: t.int(),
+    eq: t.int(),
+    le: t.int(),
+    lt: t.int(),
+    ge: t.int(),
+    gt: t.int(),
+    in: t.intList()
+  })
+});
+
 builder.queryType({});
